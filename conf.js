cd554e4135ec6c904fd27e5771ffee41059cc42b
Tetiana Koziuk <tetiana.koziuk@eleks.com>
2018-08-29 17:54:57 +0300

    first commit

---
+++ conf.js
@@ -0,0 +1,12 @@
+exports.config = {
+    framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['spec.js'],
+    capabilities:{
+        browserName: "firefox"
+    },
+   
+//    function onPrepare() {
+//         console.log("Start of tests execution!");
+//     }
+}
